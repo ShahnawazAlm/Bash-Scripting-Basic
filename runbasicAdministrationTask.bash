f3393a8fa030de91c3121fe7e45df5911cad3a70
ShahnawazAlm <shahnawaz04alm@gmail.com>
2023-07-17 12:59:43 +0545

    Initial Commit Basic Bash-Scripting

---
+++ runbasicAdministrationTask.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Author: Shahnawaz Alam
+# Date: June 16 2023
+# Description: Basic tasks of an Admin
+
+echo # this echo command without any value will print empty line
+
+echo this script will run basic administrative command as a admin
+
+echo
+
+df -h
+
+echo
+
+free -m
+
+echo
+
+duf
+
+echo
+
+uptime
+
+echo
+
+iostat
+
+echo
+
+atop | head -10
---
+++ runbasictask.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Author: Shahnawaz Alam
+# Date : june 16 2023
+# Description: Run basic linux task
+
+echo #this will print empty line
+
+echo This Script will run a few basic tasks
+
+echo #this will print empty line
+
+pwd
+echo #this will print empty line
+
+ls
+echo # this will print empty  line
+
+whoami
+echo # this will print empty line
+
+date
+echo #this will print empty lie
+
+cal
+echo #this will print empty line
+
+mkdir Directary
+
+touch File
+
+echo #this will print empty line
+
+echo End of  Script
+
